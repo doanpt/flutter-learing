186a7c9fe3fa68bee8042072a7673c716d758f6a
Trung Doan <doanpt93cntt@gmail.com>
2021-02-25 22:56:03 +0700

    add route and empty product detail screen

---
+++ lib/screens/product_detail_screen.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class ProductDetailScreen extends StatelessWidget {
+  static const routeName = "/product_detail";
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Center(
+        child: Text('Detail of Product'),
+      ),
+    );
+  }
+}
