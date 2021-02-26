400be09628c6033b18ea6038de7eb7433b0af5c4
Trung Doan <doanpt93cntt@gmail.com>
2021-02-26 22:50:50 +0700

    use Consumer to rebuild a part of widget instead of the whole

--- lib/widgets/product_item.dart
+++ lib/widgets/product_item.dart
@@ -6,40 +6,41 @@ import 'package:provider/provider.dart';
 class ProductItem extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    final product = Provider.of<Product>(context);
-    print("product is $product");
-    return ClipRRect(
-      borderRadius: BorderRadius.circular(10.0),
-      child: InkWell(
-        onTap: () {
-          Navigator.of(context).pushNamed(
-            ProductDetailScreen.routeName,
-            arguments: product.id,
-          );
-        },
-        child: GridTile(
-          child: Image.network(
-            product.imageUrl,
-            fit: BoxFit.cover,
-          ),
-          footer: GridTileBar(
-            backgroundColor: Colors.black87,
-            title: Text(
-              product.title,
-              textAlign: TextAlign.center,
+    print("rebuilds");
+    return Consumer<Product>(
+      builder: (context, product, child) => ClipRRect(
+        borderRadius: BorderRadius.circular(10.0),
+        child: InkWell(
+          onTap: () {
+            Navigator.of(context).pushNamed(
+              ProductDetailScreen.routeName,
+              arguments: product.id,
+            );
+          },
+          child: GridTile(
+            child: Image.network(
+              product.imageUrl,
+              fit: BoxFit.cover,
             ),
-            trailing: IconButton(
-              icon: Icon(
-                product.isFavorite ? Icons.favorite : Icons.favorite_border,
+            footer: GridTileBar(
+              backgroundColor: Colors.black87,
+              title: Text(
+                product.title,
+                textAlign: TextAlign.center,
+              ),
+              trailing: IconButton(
+                icon: Icon(
+                  product.isFavorite ? Icons.favorite : Icons.favorite_border,
+                  color: Theme.of(context).accentColor,
+                ),
+                onPressed: () {
+                  product.toggleFavorite();
+                },
+              ),
+              leading: Icon(
+                Icons.shopping_cart,
                 color: Theme.of(context).accentColor,
               ),
-              onPressed: () {
-                product.toggleFavorite();
-              },
-            ),
-            leading: Icon(
-              Icons.shopping_cart,
-              color: Theme.of(context).accentColor,
             ),
           ),
         ),
