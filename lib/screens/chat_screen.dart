1c4d70e4ef0556d855ca5e5cdf8c805e0dc4c908
Trung Doan <doanpt93cntt@gmail.com>
2021-03-03 09:53:20 +0700

    build conversation list

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_chat_app/models/chat_user.dart';
+import 'package:flutter_chat_app/widgets/conversation_list.dart';
 
 class ChatScreen extends StatefulWidget {
   @override
@@ -6,6 +8,49 @@ class ChatScreen extends StatefulWidget {
 }
 
 class _ChatScreenState extends State<ChatScreen> {
+  List<ChatUsers> chatUsers = [
+    ChatUsers(
+        name: "Jane Russel",
+        messageText: "Awesome Setup",
+        imageURL: "assets/images/image1.jpeg",
+        time: "Now"),
+    ChatUsers(
+        name: "Glady's Murphy",
+        messageText: "That's Great",
+        imageURL: "assets/images/image2.jpeg",
+        time: "Yesterday"),
+    ChatUsers(
+        name: "Jorge Henry",
+        messageText: "Hey where are you?",
+        imageURL: "assets/images/image3.jpeg",
+        time: "31 Mar"),
+    ChatUsers(
+        name: "Philip Fox",
+        messageText: "Busy! Call me in 20 mins",
+        imageURL: "assets/images/image4.jpeg",
+        time: "28 Mar"),
+    ChatUsers(
+        name: "Debra Hawkins",
+        messageText: "Thankyou, It's awesome",
+        imageURL: "assets/images/image5.jpeg",
+        time: "23 Mar"),
+    ChatUsers(
+        name: "Jacob Pena",
+        messageText: "will update you in evening",
+        imageURL: "assets/images/image6.jpeg",
+        time: "17 Mar"),
+    ChatUsers(
+        name: "Andrey Jones",
+        messageText: "Can you please share the file?",
+        imageURL: "assets/images/image1.jpeg",
+        time: "24 Feb"),
+    ChatUsers(
+        name: "John Wick",
+        messageText: "How are you?",
+        imageURL: "assets/images/image2.jpeg",
+        time: "18 Feb"),
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -74,6 +119,21 @@ class _ChatScreenState extends State<ChatScreen> {
                       borderSide: BorderSide(color: Colors.grey.shade100)),
                 ),
               ),
+            ),
+            ListView.builder(
+              itemCount: chatUsers.length,
+              shrinkWrap: true,
+              padding: EdgeInsets.only(top: 16),
+              physics: NeverScrollableScrollPhysics(),
+              itemBuilder: (context, index) {
+                return ConversationList(
+                  name: chatUsers[index].name,
+                  messageText: chatUsers[index].messageText,
+                  imageUrl: chatUsers[index].imageURL,
+                  time: chatUsers[index].time,
+                  isMessageRead: (index == 0 || index == 3) ? true : false,
+                );
+              },
             ),
           ],
         ),
