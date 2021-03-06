d96894db5c7be2490ca6ccf46578e811fef7d4e0
Trung Doan <doanpt93cntt@gmail.com>
2021-03-06 11:45:01 +0700

    add welcome screen

---
+++ lib/screens/welcome_screen.dart
@@ -0,0 +1,84 @@
+import 'package:flutter/material.dart';
+
+class WelcomeScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+      ),
+      backgroundColor: Theme.of(context).primaryColor,
+      body: Column(
+        mainAxisSize: MainAxisSize.max,
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Container(
+                width: 150,
+                height: 150,
+                child: CircleAvatar(
+                  radius: 75,
+                  backgroundImage: AssetImage('assets/images/flutter_blue.png'),
+                  // child: ClipOval(
+                  //   child: Image.asset(
+                  //     'assets/images/flutter_blue.png',
+                  //   ),
+                  // ),
+                  // backgroundImage: AssetImage('assets/images/dart.png'),
+                ),
+              ),
+            ],
+          ),
+          SizedBox(
+            height: 20,
+          ),
+          Text(
+            'Welcome to Flutter!',
+            style: TextStyle(
+              fontSize: 28,
+              color: Colors.white,
+            ),
+          ),
+          SizedBox(height: 20),
+          Text(
+            'A one-stop portal for you to learn the latest technologies from SCRATCH',
+            textAlign: TextAlign.center,
+            style: TextStyle(color: Colors.white, fontSize: 16),
+          ),
+          SizedBox(
+            height: 30,
+          ),
+          Container(
+            margin: EdgeInsets.symmetric(horizontal: 20),
+            child: MaterialButton(
+              onPressed: () {},
+              elevation: 0,
+              color: Theme.of(context).primaryColorLight,
+              height: 52,
+              padding: EdgeInsets.symmetric(horizontal: 20),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    'Get Started',
+                    style: TextStyle(
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 26),
+                  ),
+                  Icon(
+                    Icons.navigate_next,
+                    color: Colors.white,
+                  )
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
