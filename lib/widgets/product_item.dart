82de9065bcd8759ab8cf6a6b0c0a1818ca24f247
Trung Doan <doanpt93cntt@gmail.com>
2021-04-09 15:57:08 +0700

    Using Hero Transition to add animation between 2 views in 2 pages with same

--- lib/widgets/product_item.dart
+++ lib/widgets/product_item.dart
@@ -21,10 +21,13 @@ class ProductItem extends StatelessWidget {
             );
           },
           child: GridTile(
-            child: FadeInImage(
-              placeholder: AssetImage('assets/images/product-placeholder.png'),
-              image: NetworkImage(product.imageUrl),
-              fit: BoxFit.cover,
+            child: Hero(
+              tag: product.id,
+              child: FadeInImage(
+                placeholder: AssetImage('assets/images/product-placeholder.png'),
+                image: NetworkImage(product.imageUrl),
+                fit: BoxFit.cover,
+              ),
             ),
             footer: GridTileBar(
               backgroundColor: Colors.black87,
