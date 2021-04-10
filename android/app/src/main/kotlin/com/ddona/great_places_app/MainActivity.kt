8df15a1ec9d6922349de59e44d5ae3e773276295
Trung Doan <doanpt93cntt@gmail.com>
2021-04-10 10:56:51 +0700

    Init empty flutter pr

---
+++ android/app/src/main/kotlin/com/ddona/great_places_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.ddona.great_places_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ android/app/src/main/kotlin/com/ddona/greatplacesapp/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.ddona.greatplacesapp
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
