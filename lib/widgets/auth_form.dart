e9b301eebccf75fc568458def1c6308c28784dc3
Trung Doan <doanpt93cntt@gmail.com>
2021-04-16 17:42:14 +0700

    add validation for text form field

--- lib/widgets/auth_form.dart
+++ lib/widgets/auth_form.dart
@@ -6,6 +6,23 @@ class AuthForm extends StatefulWidget {
 }
 
 class _AuthFormState extends State<AuthForm> {
+  final _formKey = GlobalKey<FormState>();
+
+  void trySubmit() {
+    final isValid = _formKey.currentState.validate();
+    FocusScope.of(context).unfocus();
+    if (isValid) {
+      _formKey.currentState.save();
+      print('Current is $_isLogin with $username - $password - $email');
+      //
+    }
+  }
+
+  bool _isLogin = true;
+  String username;
+  String password;
+  String email;
+
   @override
   Widget build(BuildContext context) {
     return Center(
@@ -24,36 +41,74 @@ class _AuthFormState extends State<AuthForm> {
             child: Padding(
               padding: EdgeInsets.all(16.0),
               child: Form(
+                key: _formKey,
                 child: Column(
                   mainAxisSize: MainAxisSize.min,
                   children: [
                     TextFormField(
+                      key: ValueKey("email"),
+                      validator: (value) {
+                        if (value == null || !value.contains('@')) {
+                          return 'Email is invalid';
+                        }
+                        return null;
+                      },
                       keyboardType: TextInputType.emailAddress,
                       decoration: InputDecoration(
                         labelText: 'Email address',
                       ),
+                      onSaved: (value) {
+                        email = value;
+                      },
                     ),
-                    TextFormField(
-                      decoration: InputDecoration(
-                        labelText: 'Username',
+                    if (!_isLogin)
+                      TextFormField(
+                        key: ValueKey("username"),
+                        validator: (value) {
+                          if (value == null || value.length < 4) {
+                            return 'Username is invalid';
+                          }
+                          return null;
+                        },
+                        decoration: InputDecoration(
+                          labelText: 'Username',
+                        ),
+                        onSaved: (value) {
+                          username = value;
+                        },
                       ),
-                    ),
                     TextFormField(
+                      key: ValueKey("password"),
+                      validator: (value) {
+                        if (value == null || value.length < 6) {
+                          return 'Password is invalid';
+                        }
+                        return null;
+                      },
                       decoration: InputDecoration(
                         labelText: 'Password',
                       ),
                       obscureText: true,
+                      onSaved: (value) {
+                        password = value;
+                      },
                     ),
                     SizedBox(
                       height: 20,
                     ),
                     ElevatedButton(
-                      onPressed: () {},
-                      child: Text('Login'),
+                      onPressed: trySubmit,
+                      child: _isLogin ? Text('Login') : Text('Register'),
                     ),
                     TextButton(
-                      onPressed: () {},
-                      child: Text('Create new account'),
+                      onPressed: () {
+                        setState(() {
+                          _isLogin = !_isLogin;
+                        });
+                      },
+                      child: _isLogin
+                          ? Text('Create new account')
+                          : Text('Login with account'),
                     ),
                   ],
                 ),
