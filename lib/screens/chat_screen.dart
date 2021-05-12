1e589f5cf942980dbc2eb54484a74fc14013bbf3
Trung Doan <doanpt93cntt@gmail.com>
2021-05-12 22:20:49 +0700

    add firebase messaging to android

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -1,10 +1,25 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:talk/widgets/chat/message.dart';
 import 'package:talk/widgets/chat/new_message.dart';
 
-class ChatScreen extends StatelessWidget {
+class ChatScreen extends StatefulWidget {
+  @override
+  _ChatScreenState createState() => _ChatScreenState();
+}
+
+class _ChatScreenState extends State<ChatScreen> {
+
+  @override
+  void initState() {
+    FirebaseMessaging.instance.requestPermission(alert: true);
+    FirebaseMessaging.onMessage.listen((event) {
+      print('event ${event.data}');
+    });
+    super.initState();
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
