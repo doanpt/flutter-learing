9d984a640330b5c026244a509c52a0861a952ac6
Trung Doan <doanpt93cntt@gmail.com>
2021-10-07 20:31:50 +0700

    Display lock icon on right door

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -8,12 +8,27 @@ class HomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        child: Stack(children: [
-          SvgPicture.asset(
-            'assets/icons/Car.svg',
-            width: double.infinity,
-          ),
-        ]),
+        child: LayoutBuilder(
+          builder: (BuildContext context, BoxConstraints constraints) {
+            return Stack(
+              alignment: Alignment.center,
+              children: [
+                Padding(
+                  padding: EdgeInsets.symmetric(
+                      vertical: constraints.maxHeight * 0.1),
+                  child: SvgPicture.asset(
+                    'assets/icons/Car.svg',
+                    width: double.infinity,
+                  ),
+                ),
+                Positioned(
+                  right: constraints.maxWidth * 0.05,
+                  child: SvgPicture.asset('assets/icons/door_lock.svg'),
+                ),
+              ],
+            );
+          },
+        ),
       ),
     );
   }
