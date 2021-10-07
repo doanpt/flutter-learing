ba2134ccf147d16e206f8347d4f176c3bf42e58a
Trung Doan <doanpt93cntt@gmail.com>
2021-10-07 20:35:24 +0700

    Change Icon lock/unlock when tap to lock image

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,35 +1,47 @@
+import 'package:car_control/controller/home_controller.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 
 class HomeScreen extends StatelessWidget {
-  const HomeScreen({Key? key}) : super(key: key);
+  HomeScreen({Key? key}) : super(key: key);
+  final HomeController _homeController = HomeController();
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: SafeArea(
-        child: LayoutBuilder(
-          builder: (BuildContext context, BoxConstraints constraints) {
-            return Stack(
-              alignment: Alignment.center,
-              children: [
-                Padding(
-                  padding: EdgeInsets.symmetric(
-                      vertical: constraints.maxHeight * 0.1),
-                  child: SvgPicture.asset(
-                    'assets/icons/Car.svg',
-                    width: double.infinity,
-                  ),
-                ),
-                Positioned(
-                  right: constraints.maxWidth * 0.05,
-                  child: SvgPicture.asset('assets/icons/door_lock.svg'),
-                ),
-              ],
-            );
-          },
-        ),
-      ),
+    return AnimatedBuilder(
+      builder: (ctx, snapshot) {
+        return Scaffold(
+          body: SafeArea(
+            child: LayoutBuilder(
+              builder: (BuildContext context, BoxConstraints constraints) {
+                return Stack(
+                  alignment: Alignment.center,
+                  children: [
+                    Padding(
+                      padding: EdgeInsets.symmetric(
+                          vertical: constraints.maxHeight * 0.1),
+                      child: SvgPicture.asset(
+                        'assets/icons/Car.svg',
+                        width: double.infinity,
+                      ),
+                    ),
+                    Positioned(
+                      right: constraints.maxWidth * 0.05,
+                      child: GestureDetector(
+                        onTap: _homeController.updateRightDoorLock,
+                        child: _homeController.isRightDoorLock
+                            ? SvgPicture.asset('assets/icons/door_lock.svg')
+                            : SvgPicture.asset('assets/icons/door_unlock.svg'),
+                      ),
+                    ),
+                  ],
+                );
+              },
+            ),
+          ),
+        );
+      },
+      animation: _homeController,
     );
   }
 }
