8d84beb11399f5b938fb3ecf11a78f6ce89568fa
Trung Doan <doanpt93cntt@gmail.com>
2021-10-17 11:07:24 +0700

    Load images to display

--- lib/my_detail_page.dart
+++ lib/my_detail_page.dart
@@ -15,6 +15,24 @@ class DetailPage extends StatefulWidget {
 }
 
 class _DetailPageState extends State<DetailPage> {
+  List img = [];
+
+  void _readData() async {
+    await DefaultAssetBundle.of(context).loadString("json/img.json").then(
+          (s) => {
+            setState(() {
+              img = json.decode(s);
+            })
+          },
+        );
+  }
+
+  @override
+  void initState() {
+    _readData();
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     double height = MediaQuery.of(context).size.height;
@@ -314,7 +332,7 @@ class _DetailPageState extends State<DetailPage> {
             ),
             //images
             Stack(children: [
-              for (int i = 0; i < 5; i++)
+              for (int i = 0; i < img.length; i++)
                 Positioned(
                   top: 590,
                   left: (20 + i * 35).toDouble(),
@@ -324,7 +342,7 @@ class _DetailPageState extends State<DetailPage> {
                     decoration: BoxDecoration(
                       borderRadius: BorderRadius.circular(25),
                       image: DecorationImage(
-                        image: AssetImage("img/background.jpg"),
+                        image: AssetImage(img[i]['img']),
                         fit: BoxFit.cover,
                       ),
                     ),
