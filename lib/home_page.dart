ae710f3c94d8eabf05f91c82cdd5a42a2148161b
Trung Doan <doanpt93cntt@gmail.com>
2021-10-17 16:20:16 +0700

    Implement training app bar

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:video_player_app/colors.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -10,6 +11,54 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      backgroundColor: AppColor.homePageBackground,
+      body: SafeArea(
+        child: Container(
+          padding: EdgeInsets.only(left: 30.0, top: 10, right: 30),
+          child: Column(
+            children: [
+              Row(
+                children: [
+                  Text(
+                    "Training",
+                    style: TextStyle(
+                      fontSize: 30,
+                      color: AppColor.homePageTitle,
+                      fontWeight: FontWeight.w700,
+                    ),
+                  ),
+                  Spacer(),
+                  Icon(
+                    Icons.arrow_back_ios,
+                    size: 20,
+                    color: AppColor.homePageIcons,
+                  ),
+                  SizedBox(
+                    width: 10,
+                  ),
+                  Icon(
+                    Icons.calendar_today_outlined,
+                    size: 20,
+                    color: AppColor.homePageIcons,
+                  ),
+                  SizedBox(
+                    width: 10,
+                  ),
+                  Icon(
+                    Icons.arrow_forward_ios,
+                    size: 20,
+                    color: AppColor.homePageIcons,
+                  ),
+                ],
+              ),
+              SizedBox(
+                height: 30,
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
